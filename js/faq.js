1d8ce0ff4663b23994e4d4dcb9715ee3cdc46b09
Maciej Nowakowski <m.nowakowskipl@gmail.com>
2014-03-22 15:03:54 +0000

    jQuery working with FAQs in Safari

--- js/faq.js
+++ js/faq.js
@@ -1,22 +1,19 @@
-$(document).ready(function(e) {
+var $faq = $("#left-menu");ß
 
-var $faq = $('#left-menu');
-
-	$faq.find('a').on('click', function(e){
-		e.preventDefault();
-		
-		var $answer_set = $('#answer-set');
-		
-		switch($(this).attr('href')) {
-			case 'planning.html' :
-				$answer_set.load('faq/planning.html');
-				break;
-			case 'getting-started.html' :
-				$answer_set.load('faq/getting-started.html');
-				break;
-			case 'booking.html' :
-				$answer_set.load('faq/booking.html');
-				break;
-		}
-	}); 
-});
+$faq.find('a').on('click', function(e){
+	e.preventDefault();
+	
+	var $answerSet = $('#answer-set');
+	
+	switch($(this).attr('href')) {
+		case 'planning.html' :
+			$answerSet.load('../faq/planning.html');
+			break;
+		case 'getting-started.html' :
+			$answerSet.load('faq/getting-started.html');
+			break;
+		case 'booking.html' :
+			$answerSet.load('faq/booking.html');
+			break;
+	}
+}); 
